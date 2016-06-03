744fda9509f76f50e4716c225fdad09098253f8c
liuyuefeng <yuefeng_liu@foxmail.com>
2016-06-03 21:16:34 +0800

    first commit

---
+++ logRegres.py
@@ -0,0 +1,76 @@
+# coding=utf-8
+import numpy as np
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testset.txt')
+    for line in fr.readlines():
+        lineArr=line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+def sigmoid(intX):
+    return  1.0/(1+np.exp(-intX))
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()
+    m, n = np.shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = np.ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weight = weights + alpha*dataMatrix.transpose()*error
+    return weight
+    alpha = 0.001
+    maxCycles = 500
+    weights = one((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weight = weights + alpha*dataMatrix.transpose()*error
+    return weight
+    alpha = 0.001
+    maxCycles = 500
+    weights = one((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weight = weights + alpha*dataMatrix.transpose()*error
+    return weight
+    alpha = 0.001
+    maxCycles = 500
+    weights = one((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weight = weights + alpha*dataMatrix.transpose()*error
+    return weight
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1])
+            ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1])
+            ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2] 
+    ax.plot(x,y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
