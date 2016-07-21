074efdf4692e3795c67296cbc47d4517a9534aaa
liuyuefeng <yuefeng_liu@foxmail.com>
2016-07-21 21:32:36 +0800

    add regression

---
+++ regression/regression.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from numpy import *
+def loaddataset(fileName):
+	numFeat = len(open(fileName).readline().split('\t')) -1
+	dataMat = [];labelMat = []
+	fr = open(fileName)
+	for line in fr.readlines():
+		lineArr = []
+		curLine = line.strip().split('\t')
+		for i in range(numFeat):
+			lineArr.append(float(curLine[i]))
+		dataMat.append(lineArr)
+		labelMat.append(float(curLine[-1]))
+	return dataMat,labelMat
+def standRegres(xArr,yArr):
+	xMat = mat(xArr);yMat = mat(yArr).T
+	xTx = xMat.T*xMat
+	if linalg.det(xTx) == 0.0:
+		print 'this matrix is singular,cannot do inverse'
+		return
+	ws = xTx.I*(xMat.T*yMat)
+	return ws
+
